dff38c840e47cdfcc98b04289128d0e2069ef5c7
M. Aravind <matamaravind44@gmail.com>
2025-12-17 18:06:32 +0530

    neon

--- Java_Project/src/problem_Solving/Java_2.java
+++ Java_Project/src/problem_Solving/Java_2.java
@@ -1,5 +1,23 @@
 package problem_Solving;
 
 public class Java_2 {
+	
+	static Boolean method(int num) {
+		int sq = num * num;
+		int temp = num;
+		int sum = 0;
+		while (sq!=0) {
+			int rem = sq % 10;
+			sum = sum + rem;
+			sq/=10;
+		}
+		return sum == temp;
+	}
+
+	
+	public static void main(String[] args) {
+		System.out.println(method(9));
+		
+	}
 
 }
